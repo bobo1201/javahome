3e7717458ea27340475c522fdc84fb218d89e910
bobo <wjdqhtjd12@naver.com>
2023-01-23 23:39:19 +0900

    23/01/23

---
+++ JAVA230116/src/Main_asci.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Main_asci {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.next();
+		
+		char b = (char)a.charAt(0);
+		System.out.println((int)b);
+	}
+}
---
+++ JAVA230116/src/Main_숫자의합.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main_숫자의합 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		String b = sc.next();
+		int sum = 0;
+		
+		for(int i=0; i<b.length(); i++) {
+			sum += b.charAt(i)-48;
+		}
+		
+		System.out.println(sum);
+	}
+}
