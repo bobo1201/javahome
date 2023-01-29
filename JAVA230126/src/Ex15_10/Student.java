8c4a6f202a0895292b291a98df0cead4bc7806c7
bobo <wjdqhtjd12@naver.com>
2023-01-29 22:27:08 +0900

    23/01/29 10:27

---
+++ JAVA230126/src/Ex15_10/Student.java
@@ -0,0 +1,13 @@
+package Ex15_10;
+
+public class Student {
+    public String id;
+    public int score;
+    
+    public Student(String id, int score) {
+        this.id = id;
+        this.score = score;
+    }
+    
+    
+} 
---
+++ JAVA230126/src/Main_벌집.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Main_¹úÁý {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int cnt = 0;
+		int sum = 1;
+		
+		for(int i=0; i<a; i++) {
+			cnt++;
+			sum += 6*i;
+			if(a <= sum) {
+				break;
+			}
+		}
+		
+		System.out.println(cnt);
+				
+//		1 
+//		2~7   +6
+//		8~19  +12
+//		20~37 +18
+//		38~61 +24
+	}
+}
