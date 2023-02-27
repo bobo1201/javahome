9a9a4c22d78470cddd70edc0108c07c50da289bf
bobo1201 <121841746+bobo1201@users.noreply.github.com>
2023-02-28 00:11:46 +0900

    23/02/28 00:11

---
+++ JAVA230227/src/Main_단어길이재기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main_단어길이재기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.next();
+		
+		System.out.println(a.length());
+
+	}
+
+}
