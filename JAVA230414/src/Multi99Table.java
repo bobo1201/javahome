00443b08b3b208c775009fca82ce64bfcb9c4a27
bobo1201 <121841746+bobo1201@users.noreply.github.com>
2023-04-26 23:57:05 +0900

    23/04/26 23:57

---
+++ JAVA230414/src/Multi99Table.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Multi99Table {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int no;
+
+		System.out.println("2자리의 양수를 입력하세요.");
+		
+		do {
+			System.out.print("no값 : ");
+			no = sc.nextInt();
+		} while(no < 10 || no > 99);
+		
+		System.out.println("변수 no 값은 " + no +"이 되었습니다.");
+	}
+}
